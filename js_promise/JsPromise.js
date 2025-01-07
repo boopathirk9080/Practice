72c295a1458520fefba9d41f59f1bbd4aadd5b9b
Boopathi_rk <138640788+boopathirk9080@users.noreply.github.com>
2025-01-07 15:41:08 +0530

    Create JsPromise.js

---
+++ js_promise/JsPromise.js
@@ -0,0 +1,12 @@
+const data=inputvalue=>new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        if(inputvalue)
+        resolve(inputvalue+100)
+        else
+        reject("just input a value")
+    },1000);
+})
+
+// console.log(data(22))
+const userinputvalue=22
+data(userinputvalue).then(recivedvalue=> recivedvalue+100).then(console.log).catch(console.log)
